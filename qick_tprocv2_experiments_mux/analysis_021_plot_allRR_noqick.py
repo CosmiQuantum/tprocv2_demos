894b05d168b502deb0cacb2e3b81044da1eafeba
accesani <accesani@u.northwestern.edu>
2025-04-29 13:15:49 -0500

    Updating Temperature analysis script to plot recent data pt5.

--- qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
+++ qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
@@ -1852,7 +1852,7 @@ class PlotRR_noQick:
 
             # Format the x-axis to show dates in a nice format
             # ax.set_ylim(25, 300)
-            ax.set_yticks(np.linspace(25, 500, 12))
+            ax.set_yticks(np.linspace(50, 900, 20))
 
             # start_time = datetime.datetime(2025, 4, 11, 12, 30)
             # ax.set_xlim(left=start_time)
