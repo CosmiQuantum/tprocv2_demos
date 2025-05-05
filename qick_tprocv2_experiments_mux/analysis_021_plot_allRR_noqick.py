3d2e7d70f3725f695b1615dc9555117fa50b9512
accesani <accesani@u.northwestern.edu>
2025-05-05 09:04:25 -0500

    Making Latest Qubit Temps Scatter Plot pt3.

--- qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
+++ qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
@@ -1856,8 +1856,8 @@ class PlotRR_noQick:
             ax.grid(False)
 
             # Format the x-axis to show dates in a nice format
-            ax.set_ylim(50, 950)
-            ax.set_yticks(np.linspace(50, 950, 10))
+            ax.set_ylim(50, 250)
+            ax.set_yticks(np.linspace(50, 250, 10))
 
             # start_time = datetime.datetime(2025, 4, 11, 12, 30)
             # ax.set_xlim(left=start_time)
