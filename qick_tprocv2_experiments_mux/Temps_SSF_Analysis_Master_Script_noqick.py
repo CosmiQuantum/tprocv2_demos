4b86f6a0a9eb0fb4e01e990ed4700c451829a9f6
accesani <accesani@u.northwestern.edu>
2025-05-07 18:35:28 -0500

    Editing fallback method for  qtemp calcs via ssf pt7.

--- qick_tprocv2_experiments_mux/Temps_SSF_Analysis_Master_Script_noqick.py
+++ qick_tprocv2_experiments_mux/Temps_SSF_Analysis_Master_Script_noqick.py
@@ -88,6 +88,7 @@ for full_path in paths:
             ssf_paths = [os.path.join(ssf_dir, f) for f in sorted(os.listdir(ssf_dir))]  # length==ssf_n
 
             # iterate through every round (file)
+            print(ssf_n)
             for i in range(ssf_n):
                 try:
                     theta, thresh, fidelity, ig_new, *_= ssf_ge.get_ssf_in_round(I_g, Q_g, I_e, Q_e, i)
@@ -98,7 +99,7 @@ for full_path in paths:
                 key = (ssf_paths[i], QubitIndex)
                 ig_new_cache[key] = ig_new
                 timestamp_ssf_cache[key] = ssf_dates[i]
-                print('fid: ',fidelity)
+                # print('fid: ',fidelity)
                 # print('thresh ', threshold)
                 fid_cache[key] = float(fidelity)
                 threshold_cache[key] = float(thresh)
