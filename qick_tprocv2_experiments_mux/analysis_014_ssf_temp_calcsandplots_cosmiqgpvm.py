b29747b7a4a8c13713684d37e3622568c254ce64
accesani <accesani@u.northwestern.edu>
2025-05-12 10:17:32 -0500

    Setting up scripts for Qubit Temps  3-method comparison pt10.

--- qick_tprocv2_experiments_mux/analysis_014_ssf_temp_calcsandplots_cosmiqgpvm.py
+++ qick_tprocv2_experiments_mux/analysis_014_ssf_temp_calcsandplots_cosmiqgpvm.py
@@ -574,7 +574,7 @@ class TempCalcAndPlots:
             # ax.grid(alpha=0.3)
             ax.legend()
             ax.xaxis.set_major_formatter(date_fmt)
-            plt.setp(ax.get_xticklabels(), rotation=45, labelsize=10)
+            plt.setp(ax.get_xticklabels(), rotation=45, fontsize=10)
             ax.set_ylim(50, 950)
             ax.set_yticks(np.linspace(50, 950, 10), labelsize=10)
 
