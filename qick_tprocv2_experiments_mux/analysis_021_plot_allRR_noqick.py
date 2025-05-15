7a1c87c6240f973a895e40b16add71fe1370630b
accesani <accesani@u.northwestern.edu>
2025-05-15 09:30:58 -0500

    Tweaking yaxis range in Qtemps via efRabi script pt4.

--- qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
+++ qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
@@ -1853,8 +1853,8 @@ class PlotRR_noQick:
             ax.grid(False)
 
             # Format the x-axis to show dates in a nice format
-            ax.set_ylim(15, 625)
-            ax.set_yticks(np.linspace(15, 625, 10))
+            ax.set_ylim(10, 625)
+            ax.set_yticks(np.linspace(10, 625, 10))
 
             # start_time = datetime.datetime(2025, 4, 11, 12, 30)
             # ax.set_xlim(left=start_time)
