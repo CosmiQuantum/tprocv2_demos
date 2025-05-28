36c24816727c1827bf314c5350f7904a7368f0a2
accesani <accesani@u.northwestern.edu>
2025-05-28 12:36:54 -0500

    Adding vertical lines for heater temperature steps in Qtemp vs time RPM plot pt5.

--- qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script_noqick.py
+++ qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script_noqick.py
@@ -80,7 +80,7 @@ analysis_flags = {"Qtemps_vs_time_viaSSF": False,  "Qtemps_vs_time_viaRPM": True
     # "2025-05-13",
     # "2025-05-14"
     # ]
-target_dates_qtemps_RPM = ["2025-05-08", "2025-05-09", "2025-05-10", "2025-05-11", "2025-05-12", "2025-05-13", "2025-05-14", "2025-05-15"]
+target_dates_qtemps_RPM = ["2025-05-07", "2025-05-08", "2025-05-09", "2025-05-10", "2025-05-11", "2025-05-12", "2025-05-13", "2025-05-14", "2025-05-15", "2025-05-16"]
 base_dir = "/exp/cosmiq/data/QUIET/QICK_data/run6/6transmon/TLS_Comprehensive_Study"
 
 # To re-make and save RPM RR plots
