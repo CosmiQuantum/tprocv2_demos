28f83e9cbbe5cfa86a236ebf592a28d162f05d76
OliviaSeidel <quietuser@quiet-0hw.fnal.gov>
2025-06-07 07:28:45 -0500

    after stopping FastRelax on June72025

--- qick_tprocv2_experiments_mux/fast_relaxation_excitation_measurement.py
+++ qick_tprocv2_experiments_mux/fast_relaxation_excitation_measurement.py
@@ -32,7 +32,7 @@ from analysis_optimization_report import optimization_report_ge
 # Run Configurations and Optimization Params
 
 #---- General/overarching parameters
-max_datataking_round_index = 10
+max_datataking_round_index = 1400
 n_rounds_to_save_after = 1  # how many rounds to save after
 signal = 'None'  # 'I', or 'Q' depending on where the signal is
 save_figs = False  # whether to save plots
@@ -43,8 +43,8 @@ qick_verbose = False
 debug_mode = True  # if True, errors will stop the run immediately
 live_plot = False
 study = 'FastRelEx'
-sub_study = 'source_off_fast_relaxation_parameter_testing_substudy_corrected14'
-substudy_txt_notes = 'No source. Delay 2 us and relax delay set to 5 us. 70% of nominal gain on res pulse. 500k reps and 10 rounds of fast relaxation data only.'
+sub_study = 'source_on_fast_relaxation_longexposure_substudy1'
+substudy_txt_notes = 'Cesium-137 source in hot position. Delay 2 us and relax delay set to 5 us. 100% of nominal gain on res pulse. 500k reps and 200 rounds of fast relaxation data only.'
 Qs_to_look_at = [4]  # list of qubits to process
 
 #---- Res spec parameters
