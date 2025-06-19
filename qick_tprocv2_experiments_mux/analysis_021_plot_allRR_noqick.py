a2986d761065193b72753dcab0a102f960ea39d3
accesani <accesani@u.northwestern.edu>
2025-06-19 14:42:37 -0500

    Fitting RPM heater temp sweep data to a line without dropping pointspt5.

--- qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
+++ qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
@@ -2007,8 +2007,8 @@ class PlotRR_noQick:
                     T_err = qubit_data['T_mK_err']
                     T_mK = qubit_data['T_mK']
 
-                    # Skip if relative error is ≥ 60%
-                    if T_err / T_mK >= 0.6:
+                    # Skip if relative error is ≥ 40%
+                    if T_err / T_mK >= 0.4:
                         continue
 
                     # if T_err > 150:  # skip if error is too large (for example, larger than 300mK)
