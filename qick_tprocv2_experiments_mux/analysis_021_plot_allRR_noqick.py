5db3d7c270c2dc9067e3bcf751f49e22056c1188
accesani <accesani@u.northwestern.edu>
2025-06-20 11:36:46 -0500

    Making RPM histograms pt5.

--- qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
+++ qick_tprocv2_experiments_mux/analysis_021_plot_allRR_noqick.py
@@ -2326,7 +2326,7 @@ class PlotRR_noQick:
                 # skip if either is missing or relative error is larger than threshold
                 if T_mK is None or T_err is None:
                     continue
-                if T_err / T_mK >= 0.80: #80%
+                if T_err / T_mK >= 0.15: #15%
                     continue
 
                 temp_vals.append(T_mK)
