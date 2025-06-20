59967dd4088315b4892f7c7a1322aa47828f1394
accesani <accesani@u.northwestern.edu>
2025-06-20 12:09:03 -0500

    Making RPM histograms pt6.

--- qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script_noqick.py
+++ qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script_noqick.py
@@ -52,41 +52,6 @@ comb_analysis_flags = {"Qtemps_vs_time_comb_separate_plts": False,"Qtemps_vs_tim
 ############################################################################## Set up ##############################################################################
 #-------------------------------------------- For qubit temperature calculations via rabi population measurements ---------------------------------------------------
 # Specify which dates you want to loop through. It will process all the files inside all the folders that contain these dates in their title.
-# target_dates_qtemps_RPM = [
-#     "2025-04-16",
-#     "2025-04-17",
-#     "2025-04-18",
-#     "2025-04-19",
-#     "2025-04-20",
-#     "2025-04-21", #starts source on (Co)
-#     "2025-04-22",
-#     "2025-04-23", #switched source (to Cs)
-#     "2025-04-24",
-#     "2025-04-25",
-#     "2025-04-26",
-#     "2025-04-27",
-#     "2025-04-28", #Cs source moved closer
-#     "2025-04-29",
-#     "2025-04-30",
-#     "2025-05-01",
-#     "2025-05-02",
-#     "2025-05-03",
-#     "2025-05-04", # Cs source removed. No sources in Cleanroom.
-#     "2025-05-05",
-#     "2025-05-06",
-#     "2025-05-07",
-#     "2025-05-08",
-#     "2025-05-09",
-#     "2025-05-10",
-#     "2025-05-11",
-#     "2025-05-12",
-#     "2025-05-13",
-#     "2025-05-14",
-#     "2025-05-15",
-#     "2025-05-16"
-#     ]
-
-# For data before heater temperature steps
 target_dates_qtemps_RPM = [
     "2025-04-16",
     "2025-04-17",
@@ -108,7 +73,42 @@ target_dates_qtemps_RPM = [
     "2025-05-03",
     "2025-05-04", # Cs source removed. No sources in Cleanroom.
     "2025-05-05",
-    "2025-05-06"]
+    "2025-05-06",
+    "2025-05-07",
+    "2025-05-08",
+    "2025-05-09",
+    "2025-05-10",
+    "2025-05-11",
+    "2025-05-12",
+    "2025-05-13",
+    "2025-05-14",
+    "2025-05-15",
+    "2025-05-16"
+    ]
+
+# For data before heater temperature steps
+# target_dates_qtemps_RPM = [
+#     "2025-04-16",
+#     "2025-04-17",
+#     "2025-04-18",
+#     "2025-04-19",
+#     "2025-04-20",
+#     "2025-04-21", #starts source on (Co)
+#     "2025-04-22",
+#     "2025-04-23", #switched source (to Cs)
+#     "2025-04-24",
+#     "2025-04-25",
+#     "2025-04-26",
+#     "2025-04-27",
+#     "2025-04-28", #Cs source moved closer
+#     "2025-04-29",
+#     "2025-04-30",
+#     "2025-05-01",
+#     "2025-05-02",
+#     "2025-05-03",
+#     "2025-05-04", # Cs source removed. No sources in Cleanroom.
+#     "2025-05-05",
+#     "2025-05-06"]
 
 # For data during Heater temperature steps
 # target_dates_qtemps_RPM = ["2025-05-07", "2025-05-08", "2025-05-09", "2025-05-10", "2025-05-11", "2025-05-12", "2025-05-13", "2025-05-14", "2025-05-15", "2025-05-16"]
