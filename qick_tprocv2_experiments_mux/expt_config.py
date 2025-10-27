d5a12e5449ddd04aad553c881e1c3f4452ba1f4e
accesani <accesani@u.northwestern.edu>
2025-10-27 14:23:59 -0500

    tweaking rpm reps Q6, needs more

--- qick_tprocv2_experiments_mux/expt_config.py
+++ qick_tprocv2_experiments_mux/expt_config.py
@@ -216,8 +216,8 @@ if FRIDGE == "QUIET":
         },
 
         "power_rabi_ef": {
-            "reps": 300,
-            "reps2": 900, #this is only used for the experiment that uses e-f rabi to calculate qubit temperatures.
+            "reps": 400,
+            "reps2": 1000, #this is only used for the experiment that uses e-f rabi to calculate qubit temperatures.
             "rounds": 1,
             "start": [0.0] * 6,  # [DAC units]
             "stop": [1.0] * 6,  # [DAC units]
