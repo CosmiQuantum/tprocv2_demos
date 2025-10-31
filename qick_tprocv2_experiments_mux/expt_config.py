21519c545b5162c00fe22aca1c494f76922c5ec3
accesani <accesani@u.northwestern.edu>
2025-10-31 12:45:41 -0500

    finished optimizing 18dB, moving on to 19dB

--- qick_tprocv2_experiments_mux/expt_config.py
+++ qick_tprocv2_experiments_mux/expt_config.py
@@ -216,7 +216,7 @@ if FRIDGE == "QUIET":
         },
 
         "power_rabi_ef": {
-            "reps": 300, # for Pg pulse sequence during rpm, or regular ef rabi
+            "reps": 400, # for Pg pulse sequence during rpm, or regular ef rabi
             "reps2": 1200, #this is only used for the experiment that uses e-f rabi to calculate qubit temperatures (rpm). Pe pulse sequence
             "rounds": 1,
             "start": [0.0] * 6,  # [DAC units]
--- qick_tprocv2_experiments_mux/system_config.py
+++ qick_tprocv2_experiments_mux/system_config.py
@@ -126,7 +126,7 @@ class QICK_experiment:
                 # [MHz] Freqs of Qubit e/f Transition
                 "qubit_freq_fh": [4016.3, 3450.8, 3988.44, 4292.73, 4292.73, 4833.17],
                 "qubit_freq_ftores": [4016.3, 3644.76, 3988.44, 4292.73, 4303.18, 4833.17],
-                "qubit_gain_ef":  [0.0075, 0.011, 0.015, 0.1, 0.0175, 0.028],# [0.004, 0.0075, 0.0045, 0.008, 0.01],#
+                "qubit_gain_ef":  [0.0075, 0.011, 0.015, 0.12, 0.0175, 0.028],# [0.004, 0.0075, 0.0045, 0.008, 0.01],#
                 "qubit_gain_fh": [0.001, 0.015, 0.0075, 0.1, 0.15, 0.005],
                 'qubit_gain_ftores': [1]*6,#[0.2, 0.14, 0.04, 0.17, 0.13, 0.08],
                 # [0.01, 0.05, 0.05, 0.05, 0.01, 0.5], # [DAC units] Pulse Gain
@@ -138,7 +138,7 @@ class QICK_experiment:
                 "sigma": [0.26, 0.43, 0.46, 0.42, 0.385, 1.01], # DAC 2 04/07 [us] for Gaussian Pulse (5+10 DAC atten for qubit). Updated on 7/17 by Arianna
                 #"sigma": [0.05, 0.09, 0.07, 0.065, 0.09, 0.3],  # Goal: cut sigma in half [us] for Gaussian Pulse (5+4 DAC atten for qubit)
                 # "pi_amp": [0.92, 0.87, 0.75, 0.73, 0.77, 0.78], # old RR values
-                "sigma_ef": [0.18, 0.56, 0.41, 0.41, 0.295, 0.59],  # [us] for Gaussian Pulse, #Arianna 3/27
+                "sigma_ef": [0.18, 0.59, 0.45, 0.41, 0.32, 0.62],  # [us] for Gaussian Pulse, #Arianna 3/27
                 "sigma_fh": [0.15, 0.21, 0.25, 0.29, 0.28, 0.5],  # [us] for Gaussian Pulse, #Arianna 3/27
                 "pi_amp": [0.66, 0.68, 0.65, 0.65, 0.66, 0.69], # Arianna 10/10, run 8
                 "pi_amp_ampl": [0.5942, 0.634499, 0.76542, 0.7754, 0.55393, 0.9], # Joyce 04/07 DAC 0
