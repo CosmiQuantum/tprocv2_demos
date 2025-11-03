63dbdcb458706213248864af63989837708bc65a
accesani <accesani@u.northwestern.edu>
2025-11-02 19:24:25 -0600

    modifications to analysis_master for run 7, pt9

--- qick_tprocv2_experiments_mux/analysis_master_script.py
+++ qick_tprocv2_experiments_mux/analysis_master_script.py
@@ -2,38 +2,39 @@
 import sys
 import os
 sys.path.append(os.path.abspath("/home/quietuser/Documents/GitHub/tprocv2_demos/qick_tprocv2_experiments_mux/"))
-# from section_008_save_data_to_h5 import Data_H5
-# from analysis_000_load_configs import LoadConfigs
-# from analysis_001_plot_all_RR_h5 import PlotAllRR
-# from analysis_002_res_centers_vs_time_plots import ResonatorFreqVsTime
-# from analysis_003_q_freqs_vs_time_plots import QubitFreqsVsTime
-# from analysis_004_pi_amp_vs_time_plots import PiAmpsVsTime
-# from analysis_006_T1_vs_time_plots import T1VsTime
-# from analysis_005_Qtemp_vs_time_plots import QTempsVsTime
-# from analysis_007_T2R_vs_time_plots import T2rVsTime
-# from analysis_008_T2E_vs_time_plots import T2eVsTime
+from section_008_save_data_to_h5 import Data_H5
+from analysis_000_load_configs import LoadConfigs
+from analysis_001_plot_all_RR_h5 import PlotAllRR
+from analysis_002_res_centers_vs_time_plots import ResonatorFreqVsTime
+from analysis_003_q_freqs_vs_time_plots import QubitFreqsVsTime
+from analysis_004_pi_amp_vs_time_plots import PiAmpsVsTime
+from analysis_006_T1_vs_time_plots import T1VsTime
+from analysis_005_Qtemp_vs_time_plots import QTempsVsTime
+from analysis_007_T2R_vs_time_plots import T2rVsTime
+from analysis_008_T2E_vs_time_plots import T2eVsTime
 from analysis_009_T1_hist_cumul_err_plots import T1HistCumulErrPlots
 from analysis_010_T2R_hist_cumul_err_plots import T2rHistCumulErrPlots
 from analysis_011_T2E_hist_cumul_err_plots import T2eHistCumulErrPlots
-# from analysis_012_save_run_data import SaveRunData
-# from analysis_013_update_saved_run_data_notes import UpdateNote
-# from analysis_014_temperature_calcsandplots import TempCalcAndPlots
-# from analysis_015_plot_all_run_stats import CompareRuns
-# from analysis_016_metrics_vs_temp import (ResonatorFreqVsTemp, GetThermData, QubitFreqsVsTemp,
-#                                           PiAmpsVsTemp, T1VsTemp, T2rVsTemp, T2eVsTemp)
-# from analysis_017_plot_metric_dependencies import PlotMetricDependencies
-# from analysis_018_box_whisker import PlotBoxWhisker
-# from analysis_019_allan_welch_stats_plots import AllanWelchStats
-# from section_011_qubit_temperatures_efRabi import QubitTemperatureProgram, QubitTemperatureRefProgram
-# import matplotlib.pyplot as plt
-# # from datetime import datetime
-# import datetime
-# import pytz
+from analysis_012_save_run_data import SaveRunData
+from analysis_013_update_saved_run_data_notes import UpdateNote
+from analysis_014_temperature_calcsandplots import TempCalcAndPlots
+from analysis_015_plot_all_run_stats import CompareRuns
+from analysis_016_metrics_vs_temp import (ResonatorFreqVsTemp, GetThermData, QubitFreqsVsTemp,
+                                          PiAmpsVsTemp, T1VsTemp, T2rVsTemp, T2eVsTemp)
+from analysis_017_plot_metric_dependencies import PlotMetricDependencies
+from analysis_018_box_whisker import PlotBoxWhisker
+from analysis_019_allan_welch_stats_plots import AllanWelchStats
+from section_011_qubit_temperatures_efRabi import QubitTemperatureProgram, QubitTemperatureRefProgram
+import matplotlib.pyplot as plt
+# from datetime import datetime
+import datetime
+import pytz
+
 from expt_config import expt_cfg, list_of_all_qubits, tot_num_of_qubits, FRIDGE
-# from system_config import QICK_experiment
-# import numpy as np
-# import json
-# import h5py
+from system_config import QICK_experiment
+import numpy as np
+import json
+import h5py
 # from qualang_tools.plot import Fit
 # import visdom
 ###################################################### Set These #######################################################
