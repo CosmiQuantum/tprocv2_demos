61506b2562ba803eee996fcb48f92b1286c76c8c
accesani <accesani@u.northwestern.edu>
2026-04-02 00:35:07 -0500

    removed previous period for imports, not needed. reverting, pt2

--- qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script.py
+++ qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script.py
@@ -1,12 +1,8 @@
 import sys
 import numpy as np
-
-from tprocv2_demos.qick_tprocv2_experiments_mux.analysis_014_temp_calcsandplots_cosmiqgpvm import combined_Qtemp_studies
-
 np.set_printoptions(threshold=int(1e15)) #need this so it saves absolutely everything returned from the classes
 import os
 #sys.path.append(os.path.abspath("/home/quietuser/Documents/GitHub/tprocv2_demos/qick_tprocv2_experiments_mux/"))
-
 from analysis_021_plot_allRR_noqick import QubitSpectroscopy
 from qicklab.analysis.qspec import AnaQSpec
 from qicklab.analysis.ssf import AnaSSF
