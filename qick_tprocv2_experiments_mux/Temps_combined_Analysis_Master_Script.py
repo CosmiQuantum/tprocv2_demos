16589eec26ad86cd88a0037551866f3e00e78feb
accesani <accesani@u.northwestern.edu>
2026-04-02 00:44:03 -0500

    run num list change

--- qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script.py
+++ qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script.py
@@ -724,7 +724,7 @@ elif alt_ssf_analysis_flags["iminuit_method"]:
 
 ################################################### Combined Qubit Temperature Analyses ##########################################################
 #################################### Analyses combining multiple qubit temp methods AND/OR multiple runs #########################################
-run_num_list = [4,5,6,7,8]
+run_num_list = [5,6,7,8] # for quiet, start at 5. no qtemp data for run 4
 rpm_temps_by_run = {}      # rpm_temps_by_run[run][qid] = [T_mK, ...]
 rpm_temps_errs_by_run  = {}      # matching errors
 rpm_Pe_by_run = {}      # rpm_Pe_by_run[run][qid] = [P_e, ...]
