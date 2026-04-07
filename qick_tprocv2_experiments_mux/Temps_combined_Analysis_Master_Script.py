26653bebef3ed74c35a1cf61c5d416a9a856886b
accesani <accesani@u.northwestern.edu>
2026-04-07 00:35:18 -0500

    saving for the night

--- qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script.py
+++ qick_tprocv2_experiments_mux/Temps_combined_Analysis_Master_Script.py
@@ -25,7 +25,7 @@ from analysis_007_T2R_vs_time_plots import T2rVsTime
 from analysis_008_T2E_vs_time_plots import T2eVsTime
 from AB_Paper_Analysis_Plots import boxwhisker_qtemps_per_qubit_vs_run_choice, boxwhisker_pe_per_qubit_vs_run_hybrid, boxwhisker_ssf_per_qubit_vs_run
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
-run_num = 6
+run_num = 5
 run_name = f'run{run_num}/6transmon' # this is for temps analysis, for coherence analysis it's defined in its respective section
 signal = 'None' # Do not change
 final_figure_quality = 200 # plot quality
