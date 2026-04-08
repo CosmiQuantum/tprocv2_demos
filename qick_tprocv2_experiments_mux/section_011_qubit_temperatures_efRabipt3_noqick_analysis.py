5e1e5bbdd8aaf6c45c1dfe6c963b61fd98d07142
accesani <accesani@u.northwestern.edu>
2026-04-08 00:43:10 -0500

    updated rpm plot_results() func, minor tweak canonicalization missing for Q case. added for consistency

--- qick_tprocv2_experiments_mux/section_011_qubit_temperatures_efRabipt3_noqick_analysis.py
+++ qick_tprocv2_experiments_mux/section_011_qubit_temperatures_efRabipt3_noqick_analysis.py
@@ -433,8 +433,8 @@ class Temps_EFAmpRabiExperiment:
             print("Error fitting cosine (joint IQ):", e)
             return None, None, None
 
-    def plot_results(self, I, Q, gains, config = None, fig_quality = 200, use_iminuit_instead = False, filename_ext = "",
-                     rotate_using_ssf = False, ssf_angle = None):
+    def plot_results(self, I, Q, gains, config=None, fig_quality=200, use_iminuit_instead=True, filename_ext="",
+                     rotate_using_ssf=False, ssf_angle=None):
         """
         iminuit:
         Figures out which signal is best (I or Q), fits that one first, then uses the found oscillation frequency in the first
@@ -457,7 +457,7 @@ class Temps_EFAmpRabiExperiment:
             q1_d_guess_Q = np.mean(Q)
 
             q1_b_guess = 1 / gains[-1]
-            q1_c_guess = 0 # another option is np.pi/2
+            q1_c_guess = 0  # another option is np.pi/2
 
             # Initial guesses for I curve
             q1_guess_I = [q1_a_guess_I, q1_b_guess, q1_c_guess, q1_d_guess_I]
@@ -485,10 +485,10 @@ class Temps_EFAmpRabiExperiment:
             if fit_first == 'I':
                 if use_iminuit_instead:
                     popt_I, pcov_I = self.fit_cosine_iminuit(gains, I, q1_guess_I)
-                else: # NOTE; I HAVE NOT IMPLEMENTED SHARED USE OF b or c FOR CURVEFIT
+                else:  # NOTE; I HAVE NOT IMPLEMENTED SHARED USE OF b or c FOR CURVEFIT
                     popt_I, pcov_I = curve_fit(self.cosine, gains, I, maxfev=100000, p0=q1_guess_I)
 
-                popt_I = self.canonicalize_cos_params(popt_I) # new
+                popt_I = self.canonicalize_cos_params(popt_I)  # new
                 fit_cosine_I = self.cosine(gains, *popt_I)
 
                 # Extract shared b,c from I fit
@@ -501,14 +501,16 @@ class Temps_EFAmpRabiExperiment:
                 else:  # NOTE; I HAVE NOT IMPLEMENTED SHARED USE OF b or c FOR CURVEFIT
                     popt_Q, pcov_Q = curve_fit(self.cosine, gains, Q, maxfev=100000, p0=q1_guess_Q)
 
-                popt_Q = self.canonicalize_cos_params(popt_Q) # new
+                popt_Q = self.canonicalize_cos_params(popt_Q)  # new
                 fit_cosine_Q = self.cosine(gains, *popt_Q)
 
             else:  # fit_first == 'Q'
                 if use_iminuit_instead:
                     popt_Q, pcov_Q = self.fit_cosine_iminuit(gains, Q, q1_guess_Q)
-                else: # NOTE; I HAVE NOT IMPLEMENTED SHARED USE OF b or c FOR CURVEFIT
+                else:  # NOTE; I HAVE NOT IMPLEMENTED SHARED USE OF b or c FOR CURVEFIT
                     popt_Q, pcov_Q = curve_fit(self.cosine, gains, Q, maxfev=100000, p0=q1_guess_Q)
+
+                popt_Q = self.canonicalize_cos_params(popt_Q)  # new
                 fit_cosine_Q = self.cosine(gains, *popt_Q)
 
                 # Extract shared b,c from Q fit
@@ -520,6 +522,8 @@ class Temps_EFAmpRabiExperiment:
                     popt_I, pcov_I = self.fit_cosine_iminuit(gains, I, q1_guess_I, fix_b=b_shared)
                 else:  # NOTE; I HAVE NOT IMPLEMENTED SHARED USE OF b AND c FOR CURVEFIT
                     popt_I, pcov_I = curve_fit(self.cosine, gains, I, maxfev=100000, p0=q1_guess_I)
+
+                popt_I = self.canonicalize_cos_params(popt_I)  # new
                 fit_cosine_I = self.cosine(gains, *popt_I)
 
             ax2.plot(gains, fit_cosine_Q, '-', color='red', linewidth=3, label="Fit")
@@ -535,7 +539,7 @@ class Temps_EFAmpRabiExperiment:
             ax2.set_ylabel("Q Amplitude (a.u.)", fontsize=20)
             ax2.tick_params(axis='both', which='major', labelsize=16)
 
-            #---------------------------------------------------------------------------------
+            # ---------------------------------------------------------------------------------
             # --- Compute magnitude data from I and Q ---
             magnitude_data = np.sqrt(np.array(I) ** 2 + np.array(Q) ** 2)
 
@@ -552,17 +556,22 @@ class Temps_EFAmpRabiExperiment:
             else:
                 mag_popt, mag_pcov = curve_fit(self.cosine, gains, magnitude_data, maxfev=100000, p0=mag_guess)
 
-            mag_popt = self.canonicalize_cos_params(mag_popt) # New
+            mag_popt = self.canonicalize_cos_params(mag_popt)  # New
             magnitude_fit = self.cosine(gains, *mag_popt)
 
+            # Useful for tests:
+            # this gives the distance from the origin of the fitted IQ point at each gain
+            # it is derived from the I and Q fits, but it is NOT the same as our final returned amplitude
+            # it is more like a magnitude curve constructed from the fits rather than from the raw data
+            fit_IQ = np.sqrt(fit_cosine_I ** 2 + fit_cosine_Q ** 2)  # point-by-point magnitude of the fitted IQ vector
+
             # ------------------------------- compute amplitude curve from the I and Q FITS --------------------------------------
-            fit_IQ = np.sqrt(fit_cosine_I ** 2 + fit_cosine_Q ** 2) # this constructs the point-by-point magnitude of the fitted IQ vector
-            A_I = popt_I[0] # I-curve amplitude
-            A_Q = popt_Q[0] # Q-curve amplitude
-            A_amp_IQ = np.sqrt(A_I ** 2 + A_Q ** 2) # Combined IQ Amplitude from the amplitudes of the I and Q fits
-            sigma_A_I = np.sqrt(np.diag(pcov_I))[0] # I-curve amplitude error
-            sigma_A_Q = np.sqrt(np.diag(pcov_Q))[0] # Q-curve amplitude error
-            A_amp_IQ_err = np.sqrt((A_I / A_amp_IQ) ** 2 * sigma_A_I ** 2 +(A_Q / A_amp_IQ) ** 2 * sigma_A_Q ** 2) # Combined IQ Amplitude err, no covariance term
+            A_I = popt_I[0]  # I-curve amplitude
+            A_Q = popt_Q[0]  # Q-curve amplitude
+            A_amp_IQ = np.sqrt(A_I ** 2 + A_Q ** 2)  # Combined IQ Amplitude from the amplitudes of the I and Q fits
+            sigma_A_I = np.sqrt(np.diag(pcov_I))[0]  # I-curve amplitude error
+            sigma_A_Q = np.sqrt(np.diag(pcov_Q))[0]  # Q-curve amplitude error
+            A_amp_IQ_err = np.sqrt((A_I / A_amp_IQ) ** 2 * sigma_A_I ** 2 + (A_Q / A_amp_IQ) ** 2 * sigma_A_Q ** 2)  # Combined IQ Amplitude err, no covariance term
             # ------------------------------------------------------------------------------------------------------------------------
 
             ax1.legend([f"A={A_I:.4f}+/-{sigma_A_I:.4f}"], loc='best')
@@ -581,9 +590,9 @@ class Temps_EFAmpRabiExperiment:
                 # This makes the cosine amplitude of S equal to sqrt(A_I^2 + A_Q^2) if the phases are consistent.
 
                 # if you don't have an ssf angle you could maybe use this:
-                #alpha = np.arctan2(A_Q, A_I)  # angle of the oscillation vector in IQ from the data
+                # alpha = np.arctan2(A_Q, A_I)  # angle of the oscillation vector in IQ from the data
 
-                # Build the combined 1D signal from RAW data 
+                # Build the combined 1D signal from RAW data
                 S_data = np.cos(ssf_angle) * np.asarray(I) + np.sin(ssf_angle) * np.asarray(Q)
 
                 # Fit S_data to the SAME cosine model we already use
@@ -592,7 +601,7 @@ class Temps_EFAmpRabiExperiment:
                 b_guess_S = b_shared if 'b_shared' in locals() else (1 / gains[-1])  # reuse shared b if available
                 c_guess_S = 0.0
 
-                S_guess = [a_guess_S, b_guess_S, c_guess_S, d_guess_S] # array of initial guesses to feed iminuit
+                S_guess = [a_guess_S, b_guess_S, c_guess_S, d_guess_S]  # array of initial guesses to feed iminuit
 
                 if use_iminuit_instead:
                     popt_S, pcov_S = self.fit_cosine_iminuit(gains, S_data, S_guess, fix_b=b_guess_S)
@@ -609,7 +618,7 @@ class Temps_EFAmpRabiExperiment:
                 ax3.plot(gains, S_data, '-', linewidth=2, label=f"S = Icos(theta)+Qsin(theta) (theta={ssf_angle:.3f})")
                 ax3.plot(gains, fit_cosine_S, '-', linewidth=3, label=f"Fit to S, A_S={A_S:.4f}")
 
-                #print(f"[Geerlings-style] alpha={alpha:.6f} rad, A_S={A_S:.6f} +/- {sigma_A_S:.6f}")
+                # print(f"[Geerlings-style] alpha={alpha:.6f} rad, A_S={A_S:.6f} +/- {sigma_A_S:.6f}")
             ############################################################################################
             else:
                 # --- Plot amplitude (magnitude) data and its cosine fit on the third subplot ---
@@ -619,25 +628,24 @@ class Temps_EFAmpRabiExperiment:
                 # curve made from the fits of the I + Q data
                 ax3.plot(gains, fit_IQ, '-', color='orange', linewidth=3, label=f"sqrt(I_fit**2 + Q_fit**2)")
 
-
             if config is not None:
                 fig.text(plot_middle, 0.90,
-                         f"e-f RPM Q{self.QubitIndex + 1}: "  + f", Pg: {config['reps']}*{config['rounds']} avgs, Pe: {config['reps2']}*{config['rounds']} avgs, A=sqrt(A_I**2 + A_Q**2)={A_amp_IQ:.4f}+/-{A_amp_IQ_err:.4f}",
-                         fontsize=18, ha='center', va='top') #f", {config['sigma'] * 1000} ns sigma" need to add in all qqubit sigmas to save exp_cfg before putting htis back
+                         f"e-f RPM Q{self.QubitIndex + 1}: " + f", Pg: {config['reps']}*{config['rounds']} avgs, Pe: {config['reps2']}*{config['rounds']} avgs, A=sqrt(A_I**2 + A_Q**2)={A_amp_IQ:.4f}+/-{A_amp_IQ_err:.4f}",
+                         fontsize=18, ha='center',
+                         va='top')  # f", {config['sigma'] * 1000} ns sigma" need to add in all qqubit sigmas to save exp_cfg before putting htis back
             else:
                 fig.text(plot_middle, 0.90,
                          f"e-f RPM Q{self.QubitIndex + 1}: A=sqrt(A_I**2 + A_Q**2)={A_amp_IQ:.4f}+/-{A_amp_IQ_err:.4f}",
                          fontsize=18, ha='center', va='top')
 
-
             ax3.set_xlabel("Gain (a.u.)", fontsize=20)
             if rotate_using_ssf and ssf_angle is not None:
-                ax3.set_ylabel("S(theta) (a.u.)" , fontsize=20)
+                ax3.set_ylabel("S(theta) (a.u.)", fontsize=20)
             else:
                 ax3.set_ylabel("Magnitude (a.u.)", fontsize=20)
             ax3.tick_params(axis='both', which='major', labelsize=16)
             ax3.legend(loc='best')
-            #------------------------------------------------------------------------------
+            # ------------------------------------------------------------------------------
             if self.save_figs:
                 today_date = datetime.datetime.now().strftime("%Y-%m-%d")
                 dated_folder_name = f"made_on_{today_date}"
@@ -646,22 +654,22 @@ class Temps_EFAmpRabiExperiment:
                 self.create_folder_if_not_exists(outerFolder_expt)
                 now = datetime.datetime.now()
                 formatted_datetime = now.strftime("%Y%m%d%H%M%S")
-                file_name = os.path.join(outerFolder_expt, f"{filename_ext}Q{self.QubitIndex + 1}_" + f"Qtemps_RPM_" + f"{formatted_datetime}.png")
+                file_name = os.path.join(outerFolder_expt,f"{filename_ext}Q{self.QubitIndex + 1}_" + f"Qtemps_RPM_" + f"{formatted_datetime}.png")
                 fig.savefig(file_name, dpi=fig_quality, bbox_inches='tight')
                 # print('Plots saved to this folder:',outerFolder_expt)
             plt.close(fig)
 
             fit_params = {"fit_IQ": fit_IQ,
-                        "I_fit": fit_cosine_I,
-                        "Q_fit": fit_cosine_Q,
-                        "popt_I": popt_I,
-                        "pcov_I": pcov_I,
-                        "popt_Q": popt_Q,
-                        "pcov_Q": pcov_Q,
-                        "A_I": A_I,
-                        "sigma_A_I": sigma_A_I,
-                        "A_Q": A_Q,
-                        "sigma_A_Q": sigma_A_Q}
+                          "I_fit": fit_cosine_I,
+                          "Q_fit": fit_cosine_Q,
+                          "popt_I": popt_I,
+                          "pcov_I": pcov_I,
+                          "popt_Q": popt_Q,
+                          "pcov_Q": pcov_Q,
+                          "A_I": A_I,
+                          "sigma_A_I": sigma_A_I,
+                          "A_Q": A_Q,
+                          "sigma_A_Q": sigma_A_Q}
 
             if rotate_using_ssf and ssf_angle is not None:
                 return A_S, sigma_A_S, fit_params
