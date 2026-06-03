0a9206e9404c3e1066f5d29c8c415eb01e3f8d09
accesani <accesani@u.northwestern.edu>
2026-06-03 12:06:57 -0500

    checkouts up to coherence, except for Q5

--- qick_tprocv2_experiments_mux/round_robin_benchmark.py
+++ qick_tprocv2_experiments_mux/round_robin_benchmark.py
@@ -59,7 +59,7 @@ unmask = True  # Do you want to use the unmasking feature to increase resonator 
 save_shots_gerabi = False  # save IQ shots instead of averaged IQ data? for ge rabi ?
 save_shots_efrabi = False  # NOT implemented yet in this experiment. If you want to use this add code block to ef rabi experiment.
 
-Qs_to_look_at = [4] # only list the qubits you want to do the RR for
+Qs_to_look_at = [0,1,2,3,5] # only list the qubits you want to do the RR for
 
 # Data saving info
 run_name = 'run9c'
@@ -68,7 +68,7 @@ substudy_txt_notes = ('All qubits. Initial run 9c checkouts. \n')
 
 # set which of the following you'd like to run to 'True'
 run_flags = {"tof": False, "res_spec": True, "q_spec": True, "rabi": True, "ss": False, "ss_gef": False,
-             "t1": False, "t2r": False, "t2e": False, "ef_res_spec": False, "ef_q_spec": False,
+             "t1": True, "t2r": True, "t2e": True, "ef_res_spec": False, "ef_q_spec": False,
              "rabi_pop_meas": False, "ef_Rabi": False}
 
 # For 25dB DAC,
@@ -399,7 +399,7 @@ while j < n:
                     reduce_rlx_delay_ssf = True
                     reduce_rlx_delay_ssf_to = 650
 
-                max_tries = 5
+                max_tries = 1 # 5
                 try_num = 0
                 fid_check = 0
 
