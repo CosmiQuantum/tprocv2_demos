59465613383f01d43b63a9e27e1df525901c8e96
accesani <accesani@u.northwestern.edu>
2026-06-04 22:59:24 -0500

    finished first 2D sweep

--- qick_tprocv2_experiments_mux/Combined_Optimization_Scriptsv2.py
+++ qick_tprocv2_experiments_mux/Combined_Optimization_Scriptsv2.py
@@ -32,7 +32,7 @@ list_of_all_qubits = [0,1,2,3,4,5] # for QUIET [0, 1, 2, 3, 4, 5], for NEXUS [0,
 substudy = "initial_opt_25dBDAC"
 outerFolder = os.path.join(f"/data/QICK_data/run9c/6transmon/readout_optimization/{substudy}/{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}/")
 
-opt_flags = {"res_leng_sweep": False, "2d_sweep": True}
+opt_flags = {"res_leng_sweep": True, "2d_sweep": False}
 save_figs_ss = False # do you want to save SSF pics as you run the readout optimization sweeps?
 
 def create_folder_if_not_exists(folder_path):
@@ -54,17 +54,17 @@ n_loops = 4 # Number of repetitions per length to average
 # List of qubits to measure
 Qs = [0,1,2,3,5]
 
-# For 25dB DAC
+# For 25dB DAC, 6/4/2026
 res_leng_vals = [6.25, 6.0, 8.0, 6.75, 7.0, 7.25]  # 5.63, 25dB [5.5, 6.0, 5.7, 6.8, 7.0, 8.0] , [5.6, 6.0, 5.7, 6.85, 5.0, 8.5]
-res_gain = [0.8125, 0.836, 0.95, 0.6218, 0.825, 0.94]  # 0.8125,25dB, [0.8125, 0.836, 0.915, 0.6218, 0.95, 0.97], [0.825, 0.835, 0.915, 0.634, 0.95, 0.97]
-freq_offsets = [0,0,0,0,0,0]  # -0.1500, -0.1286, -0.3000, -0.1556, 0.0, -0.0222
+res_gain = [0.8056, 0.6778, 0.7808, 0.6115, 0.825, 0.8769]  # 0.8125,25dB, [0.8125, 0.836, 0.915, 0.6218, 0.95, 0.97], [0.825, 0.835, 0.915, 0.634, 0.95, 0.97]
+freq_offsets = [-0.1200,0,-0.1600,-0.2000,0,-0.2000]  # -0.1500, -0.1286, -0.3000, -0.1556, 0.0, -0.0222
 
 optimal_lengths = [None] * 6 # creates list where the script will be storing the optimal readout lengths for each qubit. We currently have 6 qubits in total.
 res_freq_ge = [None] * 6 # creates list where the script will be storing the freq of each resonator, to use in the 2d sweep
 
 j=0 # round number, from RR code. Not really used here since we just run it once for each qubit
 
-lengs = np.arange(4.5, 8.5, 0.25)
+lengs = np.arange(5.5, 8.0, 0.1)
 
 start=time.time()
 
--- qick_tprocv2_experiments_mux/find_max_fidelity.py
+++ qick_tprocv2_experiments_mux/find_max_fidelity.py
@@ -6,7 +6,7 @@ import datetime
 
 # Directory where your HDF5 files are stored
 #For QUIET
-outerFolder = "/data/QICK_data/run9/6transmon/readout_optimization/opt_Q5_25dBDAC/2026-04-26_13-12-50/study_data/Data_h5/2D_Gain_Freq_Sweeps"
+outerFolder = "/data/QICK_data/run9c/6transmon/readout_optimization/initial_opt_25dBDAC/2026-06-04_17-02-03/study_data/Data_h5/2D_Gain_Freq_Sweeps"
 
 print('For files inside: ', outerFolder)
 
