d131e298aac05ef482174bdda97254742fd76bef
accesani <accesani@u.northwestern.edu>
2026-07-01 00:12:50 -0500

    did coherence checkouts for run 9d

--- qick_tprocv2_experiments_mux/round_robin_benchmark.py
+++ qick_tprocv2_experiments_mux/round_robin_benchmark.py
@@ -33,7 +33,7 @@ from analysis_014_temp_calcsandplots_cosmiqgpvm import SSFTempCalcAndPlots
 ################################################ Run Configurations ####################################################
 st = time.time()
 
-n = 1 # number of rounds
+n = 1000000 # number of rounds
 use_iminuit_instead = True # for fitting, curve fit when False, iminuit when True
 pre_optimize = False # ignore
 freq_offset_steps = 10 # ignore
@@ -59,18 +59,18 @@ unmask = True  # Do you want to use the unmasking feature to increase resonator 
 save_shots_gerabi = False  # save IQ shots instead of averaged IQ data? for ge rabi
 save_shots_efrabi = False  # NOT implemented yet in this experiment. If you want to use this add code block to ef rabi experiment.
 
-Qs_to_look_at = [4] # only list the qubits you want to do the RR for
+Qs_to_look_at = [0,1,2,3,4,5] # only list the qubits you want to do the RR for
 
 # Data saving info
 run_name = 'run9d'
 device_name = '6transmon'
-substudy_txt_notes = ('Checking that the resonators and qubits are alive.\n')
+substudy_txt_notes = ('Initial state of the qubits run 9d up to SSF and coherence measonly, no readout optimization..\n')
 
 # set which of the following you'd like to run to 'True'
 
 run_flags = {"tof": False, "res_spec": True, "q_spec": True, "rabi": True, "ss": True,
              "ef_res_spec": False, "ss_gef": False, "ef_q_spec": False,
-             "rabi_pop_meas": False, "ef_Rabi": False, "t1": False, "t2r": False, "t2e": False}
+             "rabi_pop_meas": False, "ef_Rabi": False, "t1": True, "t2r": True, "t2e": True}
 
 # For 25dB DAC, 6/15/2026
 res_leng_vals = [5.4, 5.8, 6.4, 6.8, 7.0, 7.0]
@@ -88,7 +88,7 @@ meas_time_RR = {}
 ################################################ Data Saving Setup ##################################################
 # Folders
 study = 'round_robin_benchmark' #qubit_checkouts, DAC_filtering_tests
-sub_study = 'initial_checkouts'
+sub_study = 'initial_batch_not_opt_25dBDAC'
 data_set = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 if not os.path.exists(f"/data/QICK_data/{run_name}/"):
